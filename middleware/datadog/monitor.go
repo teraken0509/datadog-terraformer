6c9437a0d9daa0bc6a9e03e1590b122778cd4e52
kterada0509 <kterada.0509sg@gmail.com>
2019-01-15 09:43:27 +0900

    Fix template

--- middleware/datadog/monitor.go
+++ middleware/datadog/monitor.go
@@ -23,10 +23,78 @@ func (c *Credential) GetMonitor(id int) (*datadog.Monitor, error) {
 }
 
 func PrintMonitorConfiguration(monitor *datadog.Monitor) error {
-	tmpl := template.Must(template.New("monitor.tmpl").Funcs(internal.TemplateFuncs).ParseFiles("templates/datadog/monitor.tmpl"))
+	tmpl := template.Must(template.New("monitorTemplate").Funcs(internal.TemplateFuncs).Parse(monitorTemplate))
 
 	if err := tmpl.Execute(os.Stdout, *monitor); err != nil {
 		return err
 	}
 	return nil
 }
+
+const monitorTemplate = `
+resource "datadog_monitor" "monitor_{{ .Id }}" {
+	name               = "{{ .Name }}"
+	type               = "{{ .Type }}"
+	message            = "{{ .Message | escapenl }}"
+	{{ if .Options.EscalationMessage }}
+	escalation_message = "{{ .Options.EscalationMessage }}"
+	{{ end }}
+  
+	query = "{{ .Query }}"
+  
+	{{ if .Options.Thresholds }}
+	thresholds {
+	  {{ if .Options.Thresholds.Ok }}
+	  ok                = {{ .Options.ThresholdCount.Ok }}
+	  {{ end }}
+	  {{ if .Options.Thresholds.Warning }}
+	  warning           = {{ .Options.ThresholdCount.Warning }}
+	  {{ end }}
+	  {{ if .Options.Thresholds.WarningRecovery }}
+	  warning_recovery  = {{ .Options.ThresholdCount.WarningRecovery }}
+	  {{ end }}
+	  {{ if .Options.Thresholds.Critical }}
+	  critical          = {{ .Options.ThresholdCount.Critical }}
+	  {{ end }}
+	  {{ if .Options.Thresholds.CriticalRecovery }}
+	  critical_recovery = {{ .Options.ThresholdCount.CriticalRecovery }}
+	  {{ end }}
+	}
+	{{ end }}
+  
+	notify_no_data    = {{ .Options.NotifyNoData }}
+	{{ if .Options.NewHostDelay }}
+	new_host_delay = {{ .Options.NewHostDelay }}
+	{{ end }}
+	{{ if .Options.EvaluationDelay }}
+	evaluation_delay = {{ .Options.EvaluationDelay }}
+	{{ end }}
+	{{ if .Options.NoDataTimeframe }}
+	no_data_timeframe = {{ .Options.NoDataTimeframe }}
+	{{ end }}
+	{{ if .Options.RenotifyInterval }}
+	renotify_interval = {{ .Options.RenotifyInterval }}
+	{{ end }}
+	notify_audit = {{ .Options.NotifyAudit }}
+	{{ if .Options.TimeoutH }}
+	timeout_h    = {{ .Options.TimeoutH }}
+	{{ end }}
+	include_tags = {{ .Options.IncludeTags }}
+	require_full_window = {{ .Options.RequireFullWindow }}
+	locked = {{ .Options.Locked }}
+  
+	{{ if .Options.Silenced }}
+	silenced {
+	{{ range $key, $val := .Options.Silenced }}
+	  "{{ $key }}" = {{ $val }}
+	{{ end }}
+	}
+	{{ end }}
+  
+	tags = [
+	{{ range .Tags }}
+	  "{{ . }}",
+	{{ end }}
+	]
+}
+`
