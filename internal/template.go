d7dd5b31c2f397757e441fcebbe99f6a43342d76
kterada0509 <kterada.0509sg@gmail.com>
2019-01-25 17:13:43 +0900

    Add comment for TemplateFuncs

--- internal/template.go
+++ internal/template.go
@@ -5,6 +5,7 @@ import (
 	"text/template"
 )
 
+// TemplateFuncs ...
 var TemplateFuncs = template.FuncMap{
 	"escapenl": func(text string) string {
 		return strings.Replace(template.HTMLEscapeString(text), "\n", "\\n", -1)
